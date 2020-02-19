e5a1364fd060606848c2a684e2802b16c5fdd495
free3 <free3xm@gmail.com>
2020-02-19 14:55:13 +0200

    heroku upload

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 // const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const path = require("path");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
 // app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", require("./routes"));

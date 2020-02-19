3c475f13d7a3a756dfdaf115a3734ef9d29f9707
free3 <free3xm@gmail.com>
2020-02-19 14:57:33 +0200

    update

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const path = require("path");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
-// app.use(cors());
-app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", require("./routes"));
